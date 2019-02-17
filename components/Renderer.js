215bb0cb08b6cf5e60dc93b31667958c55bba9dc
Cactice <14835424+Cactice@users.noreply.github.com>
2019-02-17 12:10:08 +0900

    remove three js

--- components/Renderer.js
+++ components/Renderer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import  {WebGLRenderer} from './threejs/src/renderers/WebGLRenderer.js'
+import  {WebGLRenderer} from 'three'
 import ReactResizeDetector from 'react-resize-detector'
 
 
